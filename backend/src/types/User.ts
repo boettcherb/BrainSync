850bbe4c1a0b91ce4de2b8dfae65654e5bcf9376
Brandon Boettcher <brandonboettcher19@gmail.com>
2026-09-13 22:04:08 -0500

    Add UserInput type

--- backend/src/types/User.ts
+++ backend/src/types/User.ts
@@ -7,3 +7,12 @@ export interface User {
     created_at:    Date;
     updated_at:    Date;
 }
+
+// UserInput data type: Used when creating or updating a User.
+// Includes only fields that users are allowed to set or modify.
+// All other fields are either unchanged or set automatically by the backend.
+export interface UserInput {
+    username:      string;
+    email:         string;
+    password_hash: string;
+}
