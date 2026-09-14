e0b601b9f9ca7ac538702546e55750e20ab5548d
Brandon Boettcher <brandonboettcher19@gmail.com>
2026-09-13 22:10:27 -0500

    Add getUserByUsername and createUser DB functions

--- backend/src/db/groupRepository.ts
+++ backend/src/db/groupRepository.ts
@@ -1,5 +1,5 @@
 import { pool } from "./pool";
-import type { User } from '../types/User';
+import type { User, UserInput } from '../types/User';
 
 export async function getUserByEmail(email: string): Promise<User | null> {
     const query = `
@@ -7,5 +7,25 @@ export async function getUserByEmail(email: string): Promise<User | null> {
           WHERE email = $1;
     `;
     const result = await pool.query<User>(query, [email]);
+    return result.rows.length === 0 ? null : result.rows[0];
+}
+
+export async function getUserByUsername(username: string): Promise<User | null> {
+    const query = `
+        SELECT * FROM users
+          WHERE username = $1;
+    `;
+    const result = await pool.query<User>(query, [username]);
+    return result.rows.length === 0 ? null : result.rows[0];
+}
+
+export async function createUser(newUser: UserInput): Promise<User | null> {
+    const query = `
+        INSERT INTO users (username, email, password_hash)
+          VALUES ($1, $2, $3)
+          RETURNING *;
+    `;
+    const values = [newUser.username, newUser.email, newUser.password_hash];
+    const result = await pool.query<User>(query, values);
     return result.rows.length === 0 ? null : result.rows[0];
 }
