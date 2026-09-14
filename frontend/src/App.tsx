72ce3807ae49fbcb5e8f6a21c78ff8704363e035
Brandon Boettcher <brandonboettcher19@gmail.com>
2026-09-14 12:45:52 -0500

    Combine user and token into AuthState in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,28 +3,30 @@ import LoginPage from './components/LoginPage';
 import { loginRequest, signupRequest } from './api/auth';
 import type { User } from './types/User';
 
+interface AuthState {
+  user: User;
+  token: String;
+}
+
 function App() {
-  const [user, setUser] = useState<User | null>(null);
-  const [token, setToken] = useState<string | null>(null);
+  const [auth, setAuth] = useState<AuthState | null>(null);
 
   async function login(email: string, password: string): Promise<void> {
       const data = await loginRequest(email, password);
-      setToken(data.token);
-      setUser(data.user);
+      setAuth({ user: data.user, token: data.token });
   }
 
   async function signup(email: string, username: string, password: string): Promise<void> {
       const data = await signupRequest(email, username, password);
-      setToken(data.token);
-      setUser(data.user);
+      setAuth({ user: data.user, token: data.token });
   }
 
-  // If the user is not logged in or the token is not available, show the login page.
-  if (!user || !token) {
+  // If the user is not logged in, show the login page.
+  if (!auth) {
     return <LoginPage login={login} signup={signup} />
   }
 
-  // If the user is logged in and the token is available, show the main content.
+  // If the user is logged in, show the main content.
   return (
     <div>
       <h1>Successfully Logged In!</h1>
