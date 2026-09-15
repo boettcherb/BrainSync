6b0a1628d81f48b9b44922ecbd703f68d20c9f03
Brandon Boettcher <brandonboettcher19@gmail.com>
2026-09-15 02:52:15 -0500

    Create the GET /groups endpoint

--- backend/src/routers/groupRouter.ts
+++ backend/src/routers/groupRouter.ts
@@ -1,11 +1,21 @@
 import express from 'express';
+import { requireAuth, type AuthenticatedRequest } from '../middleware/authMiddleware';
+import * as groupDB from '../db/groupRepository';
+import type { UserGroup } from '../types/Group';
 
 const router = express.Router();
+router.use(requireAuth); // Authenticate all endpoints in this file
 
-
-// GET /groups - Get groups the current user belongs to
-router.get("/", (req, res) => {
-    res.send("Get groups the current user belongs to");
+// GET /groups - Get groups the current user is a member of
+router.get("/", async (req: AuthenticatedRequest, res) => {
+    try {
+        const userId: string = req.user!.userId;
+        const userGroups = await groupDB.getGroupsByUserId(userId);
+        res.json(userGroups);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 });
 
 
